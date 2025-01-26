51c24121dc26bdf5a01657e63b9c9b6e3d5b21c7
Jujob <jujo.bonillo90@gmail.com>
2025-01-26 16:00:19 +0100

    Create DataErrorToText

---
+++ core/presentation/ui/src/main/kotlin/com/jujodevs/invitta/core/presentation/ui/DataErrorToText.kt
@@ -0,0 +1,37 @@
+package com.jujodevs.invitta.core.presentation.ui
+
+import com.jujodevs.invitta.core.domain.DataError
+import com.jujodevs.invitta.core.stringresources.R
+
+fun DataError.asUiText(): UiText {
+    return when (this) {
+        DataError.RemoteDatabase.DATABASE ->
+            UiText.StringResource(
+                R.string.database_data_error,
+            )
+        DataError.RemoteDatabase.UNAUTHORIZED ->
+            UiText.StringResource(
+                R.string.unauthorized_data_error,
+            )
+        DataError.RemoteDatabase.NO_INTERNET ->
+            UiText.StringResource(
+                R.string.no_internet_data_error,
+            )
+        DataError.RemoteDatabase.NO_SIGNED_IN_USER ->
+            UiText.StringResource(
+                R.string.no_signed_in_user_data_error,
+            )
+        DataError.RemoteDatabase.API_NOT_AVAILABLE ->
+            UiText.StringResource(
+                R.string.api_not_available_data_error,
+            )
+        DataError.RemoteDatabase.TOO_MANY_REQUESTS ->
+            UiText.StringResource(
+                R.string.too_many_requests_data_error,
+            )
+        DataError.RemoteDatabase.UNKNOWN ->
+            UiText.StringResource(
+                R.string.unknown_data_error,
+            )
+    }
+}
