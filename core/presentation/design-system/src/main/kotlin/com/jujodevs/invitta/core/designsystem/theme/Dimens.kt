735bbfe99084b5d1d87a6cbfbe4ac0bbedd7a00a
Jujob <jujo.bonillo90@gmail.com>
2025-01-27 22:18:56 +0100

    Create Dimens object

---
+++ core/presentation/design-system/src/main/kotlin/com/jujodevs/invitta/core/designsystem/theme/Dimens.kt
@@ -0,0 +1,18 @@
+package com.jujodevs.invitta.core.designsystem.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val extraSmall = 2.dp
+    val small = 4.dp
+    val mediumSmall = 6.dp
+    val medium = 8.dp
+    val mediumLarge = 12.dp
+    val large = 16.dp
+    val extraLarge = 24.dp
+    val size32 = 32.dp
+    val size40 = 40.dp
+    val size48 = 48.dp
+    val size56 = 56.dp
+    val size64 = 64.dp
+}
